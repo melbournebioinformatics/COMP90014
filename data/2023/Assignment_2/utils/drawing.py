e430c0d5e0401bf14fc7cc32ae91bfcbf9d4fb18
Grace Hall <graceavahall@gmail.com>
2023-08-30 14:21:36 +1000

    added supporting files for assignment 2

---
+++ data/2023/Assignment_2/utils/drawing.py
@@ -0,0 +1,65 @@
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
+from typing import Any, Optional
+
+
+def draw_suffix_trie(graph: nx.DiGraph) -> None:
+    fig = plt.figure(1, figsize=(10, 10), dpi=60)
+    pos = graphviz_layout(graph, prog="dot")
+    nx.draw(
+        graph, pos, edge_color='black', width=1, linewidths=1,
+        node_size=100, node_color='pink', alpha=0.9
+    )
+    nx.draw_networkx_edge_labels(
+        graph, pos, font_color='red', font_size=20, 
+        edge_labels={e: graph.edges[e]['label'] for e in graph.edges}
+    )
+    plt.tight_layout()
+    plt.show()  
+    # plt.savefig(filepath, format="PNG")
+
+def draw_travel_undirected(graph: nx.Graph, pos: Any) -> Any:
+    fig = plt.figure(1, figsize=(30, 30), dpi=60)
+    if not pos:
+        pos = nx.spring_layout(graph, seed=9, k=0.8, iterations=13)  
+    nx.draw_networkx_nodes(graph, pos, node_color='white', node_size=25000, edgecolors='black', linewidths=2)
+    nx.draw_networkx_edges(graph, pos, width=2)
+    nx.draw_networkx_labels(graph, pos, font_size=25, font_family="sans-serif")
+    nx.draw_networkx_edge_labels(
+        graph, pos, font_color='red', font_size=40, 
+        edge_labels={e: graph.edges[e]['label'] for e in graph.edges}
+    )
+    plt.tight_layout()
+    plt.show()
+    return pos
+
+def draw_travel_directed(graph: nx.Graph, pos: Any) -> None:
+    fig = plt.figure(1, figsize=(30, 30), dpi=60)
+    nx.draw_networkx_nodes(graph, pos, node_color='white', node_size=25000, edgecolors='black', linewidths=2)
+    nx.draw_networkx_edges(graph, pos, width=3, arrows=True, arrowstyle='-|>', arrowsize=50, min_target_margin=80)
+    nx.draw_networkx_labels(graph, pos, font_size=25, font_family="sans-serif")
+    nx.draw_networkx_edge_labels(
+        graph, pos, font_color='red', font_size=40, 
+        edge_labels={e: graph.edges[e]['label'] for e in graph.edges}
+    )
+    plt.tight_layout()
+    plt.show()
+
+def draw_evo_tree(graph: nx.DiGraph, filepath: str, iteration: Optional[int]=None) -> None:
+    if iteration is not None:
+        filepath = filepath.replace('.png', f'_{iteration}.png')
+    fig = plt.figure(iteration, figsize=(10, 10), dpi=60)
+    pos = graphviz_layout(graph, prog="dot")
+    nx.draw(
+        graph, pos, edge_color='black', width=1, linewidths=1,
+        node_size=3000, node_color='pink', alpha=0.9
+    )
+    nx.draw_networkx_labels(graph, pos, font_size=25, font_family="sans-serif")
+    nx.draw_networkx_edge_labels(
+        graph, pos, font_color='red', font_size=20, 
+        edge_labels={e: f"{graph.edges[e]['label']:0.2f}" for e in graph.edges}
+    )
+    plt.savefig(filepath, format="PNG")
---
+++ data/2023/Assignment_2/utils/formatting.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+
+
+def pretty_print_matrix(the_dict: dict[str, dict[str, float]]) -> None:
+    
+    # check that this is a dict
+    try:
+        assert(isinstance(the_dict, dict))
+    except AssertionError:
+        print('Error: input is not a dictionary.')
+        return None
+
+    names = list(the_dict.keys())
+    
+    # check that all keys are present and nested structure is correct 
+    try:
+        for name1 in names:
+            for name2 in names:
+                the_dict[name1][name2]
+    except KeyError:
+        print('Error: nested structure of dictionary is incorrect.')
+        return None
+
+    # move data to pandas dataframe for pretty printing
+    df = pd.DataFrame(index=names, columns=names)
+    for name1 in names:
+        for name2 in names:
+            df[name1][name2] = the_dict[name1][name2]
+
+    # print it
+    print(df)
